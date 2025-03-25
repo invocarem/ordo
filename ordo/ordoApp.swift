e251de004ef5940bc36f7e6bf62a5a4ee0513b8b
Chen Chen <dashtotherock@gmail.com>
2025-03-24 19:01:24 -0600

    Initial Commit

---
+++ ordo/ordoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ordoApp.swift
+//  ordo
+//
+//  Created by Chen Chen on 2025-03-24.
+//
+
+import SwiftUI
+
+@main
+struct ordoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ordoTests/ordoTests.swift
@@ -0,0 +1,16 @@
+//
+//  ordoTests.swift
+//  ordoTests
+//
+//  Created by Chen Chen on 2025-03-24.
+//
+
+import Testing
+
+struct ordoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
