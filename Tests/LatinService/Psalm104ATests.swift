9dfc6ee28ec847501ddc7d40c531903923c58ece
chenchen <dashtotherock@gmail.com>
2025-12-13 16:14:36 -0700

    minor change

--- Tests/LatinService/Psalm104ATests.swift
+++ Tests/LatinService/Psalm104ATests.swift
@@ -51,21 +51,25 @@ class Psalm104ATests: XCTestCase {
     "Glory ye in his holy name: let the heart of them rejoice that seek the Lord.",
     "Seek ye the Lord, and be strengthened: seek his face evermore.",
     "Remember his marvellous works which he hath done; his wonders, and the judgments of his mouth,",
+  
     "O ye seed of Abraham his servant, ye sons of Jacob his chosen.",
     "He is the Lord our God: his judgments are in all the earth.",
     "He hath remembered his covenant for ever: the word which he commanded to a thousand generations,",
     "Which he made to Abraham; and his oath to Isaac:",
     "And he appointed the same to Jacob for a law, and to Israel for an everlasting testament,",
+  
     "Saying: To thee will I give the land of Chanaan, the lot of your inheritance.",
     "When they were but a small number: yea very few, and sojourners therein,",
     "And they passed from nation to nation, and from one kingdom to another people.",
     "He suffered no man to hurt them: and he reproved kings for their sakes.",
     "Touch ye not my anointed: and do no evil to my prophets.",
+    
     "And he called a famine upon the land: and he broke in pieces all the support of bread.",
     "He sent a man before them: Joseph, who was sold for a slave.",
     "They humbled his feet in fetters: the iron pierced his soul, until his word came. ",
     "The word of the Lord inflamed him. The king sent, and he released him: the ruler of the people, and he set him at liberty.",
     "He made him master of his house, and ruler of all his possession,",
+    
     "That he might instruct his princes as himself, and teach his ancients wisdom."
   ]
 
